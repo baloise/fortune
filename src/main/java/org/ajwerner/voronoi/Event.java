4289db1753ab975d0c651dc08bd25bf2a736f4e5
Matthias Cullmann <matthias.cullmann@gmail.com>
2017-07-01 08:02:29 +0200

    equals & hashcode

--- src/main/java/org/ajwerner/voronoi/Event.java
+++ src/main/java/org/ajwerner/voronoi/Event.java
@@ -14,4 +14,31 @@ public class Event implements Comparable<Event> {
     public int compareTo(Event o) {
         return Point.minYOrderedCompareTo(this.p, o.p);
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((p == null) ? 0 : p.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Event other = (Event) obj;
+		if (p == null) {
+			if (other.p != null)
+				return false;
+		} else if (!p.equals(other.p))
+			return false;
+		return true;
+	}
+    
+    
 }
--- src/main/java/org/ajwerner/voronoi/Point.java
+++ src/main/java/org/ajwerner/voronoi/Point.java
@@ -57,4 +57,34 @@ public class Point implements Comparable<Point> {
     public double distanceTo(Point that) {
         return Math.sqrt((this.x - that.x)*(this.x - that.x) + (this.y - that.y)*(this.y - that.y));
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(x);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(y);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point other = (Point) obj;
+		if (Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x))
+			return false;
+		if (Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y))
+			return false;
+		return true;
+	}
+    
+    
 }
